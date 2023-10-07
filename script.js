add80758033e4dad1f0b68a632517cd6f7be5f9f
Devashish Chakraborty <devashishchakra@gmail.com>
2023-10-07 16:09:42 +0530

    Created a function which recieves the promise and displays location and date(for now) as an initial setup.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+const searchForm = document.querySelector(".searchBar > form");
+const weatherInfoCard = document.querySelector(".weatherInfoCard");
+
 async function getWeather(place){
     const request = await fetch(`https://api.weatherapi.com/v1/current.json?key=ee30b4612cb9415e8dd173426232909&q=${place}`);
     const response = await request.json();
-    console.log(response);
+    return response;
 }
 
-getWeather("Jammu");
+async function displayWeatherDetails(response){
+    const weather = await response;
+    const location = weatherInfoCard.querySelector(".location");
+    const datetime = weatherInfoCard.querySelector(".datetime");
+
+    location.textContent = `${weather.location.name}, ${weather.location.country}`;
+    datetime.textContent = `${weather.current.last_updated}`;
+}
+
+const weather = getWeather("New Delhi");
+displayWeatherDetails(weather);
+
+searchForm.addEventListener("submit", function(e){
+    console.log(getWeather(place.value));
+    e.preventDefault();
+});

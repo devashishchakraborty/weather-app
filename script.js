317119e153c5458fbde85e431dd39d75814fa26d
Devashish Chakraborty <devashishchakra@gmail.com>
2023-10-09 15:35:55 +0530

    Removed the getWeather function, instead covered the entire process in a new function including DOM manipulation.

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const searchForm = document.querySelector(".searchBar > form");
 const weatherInfoCard = document.querySelector(".weatherInfoCard");
 
-async function getWeather(place){
+
+async function displayWeatherDetails(place) {
     const request = await fetch(`https://api.weatherapi.com/v1/current.json?key=ee30b4612cb9415e8dd173426232909&q=${place}`);
-    const response = await request.json();
-    return response;
+    const weather = await request.json();
+    const conditionIconSrc = weather.current.condition.icon.replace("//cdn.weatherapi.com", "images");
+
+    weatherInfoCard.innerHTML = `
+        <div class="location">${weather.location.name}, ${weather.location.country}</div>
+        <div class="datetime">${weather.current.last_updated.split(" ").join(" | ")}</div>
+        <div class="condition">
+            <img class="icon" src="${conditionIconSrc}">
+            <div>${weather.current.condition.text}</div>
+        </div>
+        <div class="actualTemp">${weather.current.temp_c}°C</div>
+        <div class="feelsLikeTemp">Feels Like ${weather.current.feelslike_c}°C</div>
+        <div class="humidity">Humidity ${weather.current.humidity}%</div>
+        <div class="cloud">Cloudiness ${weather.current.cloud}%</div>
+        <div class="windSpeed">Wind Speed ${weather.current.wind_kph}km/h</div>
+        <div class="windDirection">Wind Direction ${weather.current.wind_dir}</div>
+        <div class="uv">UV Index ${weather.current.uv}</div>
+    `
+
 }
 
-async function displayWeatherDetails(response){
-    const weather = await response;
-    const location = weatherInfoCard.querySelector(".location");
-    const datetime = weatherInfoCard.querySelector(".datetime");
+displayWeatherDetails("New Delhi");
 
-    location.textContent = `${weather.location.name}, ${weather.location.country}`;
-    datetime.textContent = `${weather.current.last_updated}`;
-}
-
-const weather = getWeather("New Delhi");
-displayWeatherDetails(weather);
-
-searchForm.addEventListener("submit", function(e){
-    console.log(getWeather(place.value));
+searchForm.addEventListener("submit", function (e) {
+    console.log(displayWeatherDetails(place.value));
     e.preventDefault();
 });
